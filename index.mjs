17ebcb3c6bd6fb86d6298a3492b9bf918abebbfd
aman <ay845790@gmail.com>
2026-04-16 17:33:13 +0530

    create  andcheck login  route

--- index.mjs
+++ index.mjs
@@ -1,13 +1,26 @@
-// "console.log("server');
-
 import express from "express";
+
 const app = express();
 const port = 3000;
 
+app.use(express.json());
+
+// Home route
 app.get("/", (req, res) => {
-  res.send("Hello World!");
+  res.json({ greet: "Hello World" });
+});
+
+// LOGIN route (FIXED)
+app.post("/login", (req, res) => {
+  res.json({ login: "login success" });
+});
+
+// SIGNUP route (FIXED)
+app.post("/signup", (req, res) => {
+  console.log(req.body);
+  res.json({ signup: "signup success" });
 });
 
 app.listen(port, () => {
-  console.log(`server started ${port}`);
+  console.log(`server started on port ${port}`);
 });
